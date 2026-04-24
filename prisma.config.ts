87031550bb4bcf208763ada7cc666d073a0d5302
Wulius <werbesan@gmail.com>
2026-04-24 15:46:02 +0200

    fix: add DATABASE_URL to schema.prisma, simplify prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,5 @@
 import { defineConfig } from "prisma/config";
-import * as dotenv from "dotenv";
-import path from "path";
-
-dotenv.config({ path: path.join(process.cwd(), ".env") });
-dotenv.config({ path: path.join(process.cwd(), ".env.local") });
 
 export default defineConfig({
   schema: "./prisma/schema.prisma",
-  datasource: {
-    url: process.env.DATABASE_URL!,
-  },
 });
